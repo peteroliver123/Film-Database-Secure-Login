32844eb70b51731695474c439c40357f512a95f7
peteroliver123 <Brackenp18@gmail.com>
2025-08-01 18:01:35 +0100

    Full Security Encapsulation Working (SecureQuit to do)

---
+++ pythonFiles/filmData/src/main.py
@@ -0,0 +1,79 @@
+# Connecting to Server
+import pymysql
+import security
+
+def search_fun(session):
+    print("Welcome to Search!")
+    print("Separate multiple entries with spaces")
+    film_name = input("Enter film name (to search by name or press enter to leave blank)\n")
+    age_rating = input("Enter any amount of age ratings (to search by age or press enter to leave blank)\n")
+    id_number = input("Enter an id (to search by id or press enter to leave blank)\n")
+    location = input("Enter any amount of locations (to search by location or press enter to leave blank)\n")
+
+    print(f"You entered:\n{film_name}\n{age_rating}\n{id_number}\n{location}")
+    while True :
+        command = input("If you are happy with these type GO, type REENTER to re-enter and type MAIN to return to main menu")
+        match command:
+            case "GO":
+                #launch_search()
+                break
+            case "REENTER":
+                search_fun(session)
+                break
+            case "MAIN":
+                main_menu(session)
+                break
+
+def command_handler(command, session):
+    match command:
+        case "HELP":
+            print("Here are some sample commands")
+            print("SEARCH\nEDIT\nPROFILE\nLOGOUT\n")
+            command = input("Try entering one of these\n")
+            command_handler(command, session)
+        case "SEARCH":
+            search_fun(session)
+        case "EDIT":
+            return
+        #editFun()
+        case "PROFILE":
+            return
+        #profileFun()
+        case "LOGOUT":
+            exit("User logged out!")
+        case _:
+            command = input("Command not recognised. Try again or type HELP:\n")
+            command_handler(command, session)
+
+def main_menu(session):
+    command = input("\nHello " + session.user_name + "! How can I help you today? (Type HELP if you are new)\n").strip().upper()
+    command_handler(command, session)
+
+def launch(session):
+    print("Logging you in ...")
+    print("Current User is " + session.user_name)
+
+    main_menu(session)
+
+    # Call stored procedure getAllFilms and output result to the screen
+    # fetchmany, fetchone, fetchall
+    #cursor.callproc('getAllFilms')
+    #results = (cursor.fetchall())
+    #for row in results:
+    #     print(*row)
+
+    #cursor.callproc('getAllFilms')
+    #results2 = (cursor.fetchall())
+    #for row in results:
+    #    print(*row)
+    #conn.close()
+
+def print_welcome_message():
+    print("\nHello! Welcome to FilmManager 2.0! Login to continue\n")
+
+def main():
+    security.login()
+
+if __name__ == "__main__":
+    main()
+
---
+++ pythonFiles/filmData/src/security.py
@@ -0,0 +1,115 @@
+from sqlalchemy.testing.engines import close_open_connections
+
+import main
+import pymysql
+"""
+CurrentSession class manages the current state. Which User is logged in
+and whether they are ADMIN (and hence have access to certain privileges)
+Encapsulated code inside this class and this file for functions for
+extra security.
+"""
+class CurrentSession:
+    def __init__(self, name, is_admin, conn):
+        self.user_name = name
+        self.is_admin = is_admin
+        self.conn = conn
+        self.cursor = ""
+
+    def get_user_name(self):
+        return self.user_name
+
+    def get_is_admin(self):
+        return self.is_admin
+
+    def set_is_admin(self, boolean):
+        self.is_admin = boolean
+
+    def open_conn(self):
+        self.conn = pymysql.connect(host="localhost", user="root", password="Founders72!", database="record_boxes")
+        print("Connected to FilmDatabase!")
+        self.cursor = self.conn.cursor()
+
+    def close_conn(self):
+        self.conn.close()
+
+    def get_cursor(self):
+        return self.cursor
+
+
+def secure_quit(session, code):
+    session.close_conn()
+    quit(code)
+
+#This function creates a new user and saves the password
+def create_new_user(new_name):
+    file = open("files/passwords.txt", "a")
+    password_count = 3
+    while True:
+        new_password = input("Enter a password: ")
+        confirm_password = input("Confirm password: ")
+        if new_password != confirm_password :
+            password_count -= 1
+            if password_count == 0:
+                quit("Passwords didn't match too many times!")
+            print(f"Passwords didn't match! {password_count} attempts remaining!")
+        else :
+            file.write(new_name + " " + new_password.strip() + "\n")
+            break
+    file.close()
+    print("New Account Created! Login to continue!")
+    login()
+
+#This function reads the password file, and returns -1 if username not found
+def read_passwords(user_name):
+    file = open("files/passwords.txt", "r")
+    for row in file:
+        *name, password = row.strip().split()
+        name = " ".join(name)
+        if name == user_name:
+            file.close()
+            return password
+    file.close()
+    return -1
+
+# This function manages user login, if username doesn't exist, creates new one,
+# if it does asks for password (performs appropriate security checks) then
+# creates a session and passes this session with needed info securely encapsulated
+# to the launch function.
+def login():
+    main.print_welcome_message()
+    name = input("Enter Username: ").strip()
+    stored_password = read_passwords(name)
+    if stored_password != -1:
+        s = CurrentSession(name, False)
+        i = 3
+        while i != 0:
+            password = input("Enter Password: ").strip()
+            if password.upper() == "CANCEL" :
+                login()
+            elif password == stored_password:
+                print("Password Correct!")
+                if name == "ADMIN":
+                    manufacturer_code = "X5ry&cvgHTY6574"
+                    code = input("Enter manufacturer code to gain access (Warning 1 attempt) (Warning admin can cause damage to server):\n")
+                    if code == manufacturer_code:
+                        s.set_is_admin(True)
+                        main.launch(s)
+                    else :
+                        quit("Incorrect! Logging you out!")
+                else :
+                    main.launch(s)
+                    break
+            else :
+                i -= 1
+                print(f"Password Incorrect! {i} attempts remaining! Type CANCEL to try another name or enter correct password")
+        quit("Password wrong too many times!")
+    else:
+        while True:
+            command = input(f"Username {name} does not exist.\nType CREATE to make this a username, type CANCEL to try another name\n").upper()
+            if command == "CREATE":
+                create_new_user(name)
+                break
+            elif command == "CANCEL":
+                login()
+            else :
+                print("Invalid Command!")
